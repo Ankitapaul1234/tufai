5d595d549967e15d6a91b8d0f5fc6116aa99d9be
Ankitapaul1234 <ankitapaul273@gmail.com>
2025-02-25 10:41:43 +0530

    some ui done

--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -2,18 +2,23 @@ import React from 'react'
 import { getUserOnboardingStatus } from "@/actions/user";
 // import { redirect } from 'next/dist/server/api-utils';
 import { redirect } from "next/navigation";
+import { getIndustryInsights } from '@/actions/dashboard';
+// import DashboardView from "./_component/dashboard-view";
+import DashboardView from './_components/dashboard-view';
 
 const IndustryInsightsPage = async () => {
   
     const { isOnboarded } = await getUserOnboardingStatus();
-    
+    const insights = await getIndustryInsights();
 
   // If not onboarded, redirect to onboarding page
   // Skip this check if already on the onboarding page
   if (!isOnboarded) {
     redirect("/onboarding");
   }
-    return <div>IndustryInsightsPage</div>;
+    return <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>;
   
 }
 
